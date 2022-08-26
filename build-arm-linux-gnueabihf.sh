43c9761846d6f1eb19dda0bfef2d93ea5106a4e4
Fangjun Kuang <csukuangfj@gmail.com>
2022-08-26 12:02:58 +0800

    minor fixes

--- build-arm-linux-gnueabihf.sh
+++ build-arm-linux-gnueabihf.sh
@@ -5,5 +5,5 @@ set -x
 mkdir -p build-arm-linux-gnueabihf
 cd build-arm-linux-gnueabihf
 cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake ..
-make -j4
+make VERBOSE=1 -j4
 make install
